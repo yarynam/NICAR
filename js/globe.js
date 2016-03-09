51b87a9d3cb7fe092120e3d655cce5d5548976a8
yarynam <yaryna.my@gmail.com>
2016-03-09 09:04:30 -0600

    draft

---
+++ js/globe.js
@@ -0,0 +1,73 @@
+var width = 200,
+    height = 200;
+
+var projection = d3.geo.orthographic()
+    .translate([width / 2, height / 2])
+    .scale(width / 2 - 20)
+    .clipAngle(90)
+    .precision(0.6);
+
+var canvas = d3.select("#globe").append("canvas")
+    .attr("width", width)
+    .attr("height", height);
+
+var c = canvas.node().getContext("2d");
+
+var path = d3.geo.path()
+    .projection(projection)
+    .context(c);
+
+var participants = d3.select("#participants");
+var title = d3.select("#country");
+
+queue()
+    .defer(d3.json, "js/world-110m.json")
+    .defer(d3.tsv, "js/world-country-names2.tsv")
+    .await(ready);
+
+function ready(error, world, names) {
+  if (error) throw error;
+
+  var globe = {type: "Sphere"},
+      land = topojson.feature(world, world.objects.land),
+      countries = topojson.feature(world, world.objects.countries).features,
+      borders = topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }),
+      i = -1,
+      i2 = -1,
+      n = countries.length;
+
+  countries = countries.filter(function(d) {
+    return names.some(function(n) {
+      if (d.id == n.id) return d.name = n.name, d.number = n.number;
+    });
+  }).sort(function(a, b) {
+    return a.name.localeCompare(b.name);
+  });
+
+  (function transition() {
+    d3.transition()
+        .duration(1250)
+        .each("start", function() {
+          title.text(" from " + countries[i = (i + 1) % n].name);
+          if (countries[i2 = (i2 + 1) % n].number > 1) {
+          participants.text( countries[i2 = (i2 + 0) % n].number + " participants")
+        } else { participants.text( countries[i2 = (i2 + 0) % n].number + " participant") }
+        })
+        .tween("rotate", function() {
+          var p = d3.geo.centroid(countries[i]),
+              r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
+          return function(t) {
+            projection.rotate(r(t));
+            c.clearRect(0, 0, width, height);
+            c.fillStyle = "#d9d9d9", c.beginPath(), path(land), c.fill();
+            c.fillStyle = "#439fd8", c.beginPath(), path(countries[i]), c.fill();
+            c.strokeStyle = "#fff", c.lineWidth = .5, c.beginPath(), path(borders), c.stroke();
+            c.strokeStyle = "#d9d9d9", c.lineWidth = 1, c.beginPath(), path(globe), c.stroke();
+          };
+        })
+      .transition()
+        .each("end", transition);
+  })();
+}
+
+d3.select(self.frameElement).style("height", height + "px");
---
+++ js/nicarminutes.js
@@ -0,0 +1,45 @@
+/// --------------
+/// Based on Simple Charts Library by @ahoiin, Sebastian Sadowski
+/// --------------
+
+// define class
+function nicarMinutes (el,val, height){
+
+  var that = this;
+  var data = [ val],
+      margin = {top: 5, right: 3, bottom: 5, left: 3},
+      innerWidth = 100 - margin.left - margin.right,
+      innerHeight = height - margin.top - margin.bottom,
+      paddingEl = 7,
+      elementsInRow = 25,
+      row = 1,
+      dataset = [];
+
+  data.forEach(function(d, i) {
+    while(d--) {
+      var c = (i==0);
+      dataset.push({color:c});
+    }
+  });
+
+  var svg = d3.select(el).append("svg")
+    .attr("width", 200)
+    .attr("height", innerHeight )
+    .append("g")
+    .attr("transform", "translate(" + (margin.left ) + "," + (margin.top ) + ")");
+
+  var circle = svg.selectAll("rect")
+    .data(dataset)
+    .enter().append("rect")
+    .style("fill", "#ddd")
+    .attr('width', 2.7)
+    .attr('height',2.7)
+    .attr("x", function(d,i) { row = (i%elementsInRow == 0) ? i/elementsInRow : row; return ((i+1)-row*elementsInRow) * paddingEl;})
+    .attr("y", function(d,i) { row = (i%elementsInRow == 0) ? i/elementsInRow : row; return row * paddingEl;})
+
+   // simple animation on load: opaciy
+   circle.transition()
+    .duration(50)
+    .delay(function(d,i) { return 15 * i; })
+    .style("fill", "#31cfff")
+};
